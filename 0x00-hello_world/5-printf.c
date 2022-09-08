abe357c7cd082211160c4ee6108f810a01299ce5
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-08 16:47:10 -0700

    Printing a string of characters using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Printing a string of characters on the screen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
