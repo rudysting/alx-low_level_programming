1b3d73b174ec4f6ae0a5418a9b0bf5dd5945d977
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-09 18:13:21 -0700

    Printing Lowercase ALphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else if (last_digit < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,
 			last_digit);
 	}
 	return (0);
