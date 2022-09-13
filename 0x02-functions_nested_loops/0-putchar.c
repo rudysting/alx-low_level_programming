b69bfbf0aa605294f5dc9cc994d15eff4bd04dbe
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-13 23:00:18 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * Entry point - main program
+ * main - Entry point
  *
- * main - Print putchar
+ * Print putchar
  *
  * Return: Zero this time (0)
  */
 int main(void)
 {
 	char *c = "_putchar";
+
 	while (*c)
 	{
 		_putchar(*c);
