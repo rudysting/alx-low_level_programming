36753d31d27fa0cc403648e0e23f6cde4e96d5ac
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-14 03:23:31 +0000

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,13 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
 
-	if (n < 0)
+       	last_digit = n % 10;
+	if (last_digit < 0)
 	{
-		n = (-1 * n);
+		last_digit = (-1 * last_digit);
 	}
 	_putchar(last_digit + '0');
-	return (0);
+	return (last_digit);
 }
