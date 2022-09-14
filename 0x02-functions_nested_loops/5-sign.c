32414d76da84a26a41746f9b3b0451804779c520
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-14 01:39:58 +0000

    Detect Sign in a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ * print_sign() - program that checks for the sign of a number
+ *
+ * @n: counter for the printed numbers
+ *
+ * Return: Variable depending on the sign of the number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
