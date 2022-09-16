ec4fc79b5ae3346921cfa306c4ae550e29a223e8
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-16 20:44:49 +0000

    Print Squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,19 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar('#');
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
