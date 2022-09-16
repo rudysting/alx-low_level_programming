343d25d34203580408af969384e3daaf71fec4a5
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-16 13:55:24 +0000

    Print a line on standard output

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line in the terminal
+ *
+ *@n: No of times to print a dash in stdout
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
