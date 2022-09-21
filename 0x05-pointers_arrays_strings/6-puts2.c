1eec07dcffed0505a0821211d5fcefe98754c5cf
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-21 18:19:44 +0000

    Print every other character in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,31 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+	int l = _strlen(str);
+
+	while (i < l)
 	{
 		_putchar(*str);
-		str+= 2;
+		str += 2;
+		i += 2;
 	}
 	_putchar('\n');
+}
+/**
+ * _strlen - counts the length of a string
+ *
+ * @s: characters in the string
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
 }
