a4474ebbb90514044da154f5cc69d00537d812c2
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-23 12:20:24 +0000

    Add two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,19 +2,16 @@
 
 /**
  * rev_string - reverse array
- * @n: integer parameter
+ * @n: integer parameters
  * Return: nothing
  */
 void rev_string(char *n)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0;
 	char temp;
 
 	while (*(n + i) != '\0')
-	{
 		i++;
-	}
 	i--;
 	for (j = 0; j < i; j++, i--)
 	{
@@ -26,28 +23,20 @@ void rev_string(char *n)
 
 /**
  * infinite_add - continuously adds 2 integers
- *
  * @n1: character 1
  * @n2: character 2
  * @r: pointer to the buffer
  * @size_r: size of r
- *
  * Return: char
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0;
-	int i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0, val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
-	{
 		i++;
-	}
 	while (*(n2 + j) != '\0')
-	{
 		j++;
-	}
 	i--;
 	j--;
 	if (j >= size_r || i >= size_r)
