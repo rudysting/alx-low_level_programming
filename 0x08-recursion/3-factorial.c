ee6e2f359a61c757f2700dbc3019307746e30c76
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-09-28 16:56:11 -0700

    Finds the factorial using recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - finds the factorial
+ * @n: integer
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
