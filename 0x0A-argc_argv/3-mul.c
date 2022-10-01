a414e1c4b1e430e76c4307c79585901489547933
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-10-01 16:12:10 -0700

    Multiply argument to main

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <stdio.h>
+
+int _atoi(char *s);
+int multiply_two_nums(int num1, int num2);
+void print_number(int n);
+
+/**
+ * main - Entry point
+ * @argc: Length of @argv
+ * @argv: Array of strings of the arguments of this program
+ * Return: 0, Success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		print_number(multiply_two_nums(_atoi(argv[1]), _atoi(argv[2])));
+		putchar('\n');
+		return (0);
+	}
+	else
+	{
+		putchar('E');
+		putchar('r');
+		putchar('r');
+		putchar('o');
+		putchar('r');
+		putchar('\n');
+		return (0);
+	}
+}
+
+/**
+ * _atoi - Convert a number in a string to an integer
+ * @s: String to be converted
+ * Return: Integer converted OR (0) if none
+ */
+
+int _atoi(char *s)
+{
+	int sign_cache = 1;
+	int result = 0;
+
+	while (s[0] != '\0')
+	{
+		if (s[0] == '-')
+			sign_cache *= -1;
+		else if (s[0] >= '0' && s[0] <= '9')
+			result = (result * 10) + (s[0] - '0');
+		else if (result)
+			break;
+		s++;
+	}
+	return (result * sign_cache);
+}
+
+/**
+ * multiply_two_nums - Multiplies two numbers
+ * @num1: A number
+ * @num2: A number
+ * Return: Result of num1 * num2
+ */
+
+int multiply_two_nums(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * print_number - Prints numbers chars
+ * @n: Integers params
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,4 +3,7 @@
 int _putchar(char c);
 void print_str(char *str);
 void print_number(int num);
+int _atoi(char *s);
+int multiply_two_nums(int num1, int num2);
+void print_number(int n);
 #endif
