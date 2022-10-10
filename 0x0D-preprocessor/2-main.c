b9286805fed411a8cabcbc6fa1ae7f2279712971
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-10-10 15:44:50 -0700

    Expanding a Function anme using Macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the function
+ * @argc: count of arguments
+ * @argv: the array of arguments
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
