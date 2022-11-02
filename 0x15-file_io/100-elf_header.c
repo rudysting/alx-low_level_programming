1aa18806e3b52f7f7d96c8b78d348e69cc9ca73c
Nnamdi_Agwuh <terrynnamdi@yahoo.com>
2022-11-02 12:30:43 -0700

    File Operations

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -231,7 +231,8 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 	printf("  Entry point address:               ");
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
-		e_entry = ((e_entry << 8) & 0xFF00FF00) | ((e_entry >> 8) & 0xFF				00FF);
+		e_entry = ((e_entry << 8) & 0xFF00FF00) |
+		       	((e_entry >> 8) & 0xFF00FF);
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 	if (e_ident[EI_CLASS] == ELFCLASS32)
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,25 +48,24 @@ int main(int argc, char *argv[])
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-		{											error_file(-1, 0, argv);
-			nwr = write(file_to, buf, nchars);
-			if (nwr == -1)
-				error_file(0, -1, argv);
-		}
-		err_close = close(file_from);
-		if (err_close == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-				file_from);
-			exit(100);
-		}
-		err_close = close(file_to);
-		if (err_close == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-				file_from);
-			exit(100);
-		}
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			file_from);
+		exit(100);
+	}
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			file_from);
+		exit(100);
 	}
 	return (0);
 }
